8549973ae17e11f0126f376b7f57c0b831a4425f
94natem <nate_m@live.com>
2018-12-21 19:12:21 -0700

    Functioning Friend Finder with basic CSS, Fix friend matcher

---
+++ app/data/friends.js
@@ -0,0 +1,164 @@
+var friendsArray = [
+	{
+		name: 'Ragnar',
+		photo: 'https://vignette.wikia.nocookie.net/vikingstv/images/b/b7/Ragnar_s2.jpg/revision/latest/scale-to-width-down/310?cb=20170115224647',
+		scores: [
+			'1',
+			'3',
+			'3',
+			'1',
+			'3',
+			'3',
+			'1',
+			'1',
+			'1',
+			'3'
+		]
+	},
+	{
+		name: 'Floki',
+		photo: 'https://vignette.wikia.nocookie.net/vikingstv/images/5/56/Floki_hd.jpg/revision/latest?cb=20180127050230',
+		scores: [
+			'2',
+			'2',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'2',
+			'1',
+			'3'
+		]
+	},
+	{
+		name: 'Lagertha',
+		photo: 'https://vignette.wikia.nocookie.net/vikingstv/images/5/51/Screen_Shot_2018-12-20_at_7.01.41_PM.png/revision/latest/scale-to-width-down/310?cb=20181221030718',
+		scores: [
+			'2',
+			'2',
+			'3',
+			'1',
+			'1',
+			'3',
+			'3',
+			'1',
+			'1',
+			'1'
+		]
+	},
+	{
+		name: 'Athelstan',
+		photo: 'hhttps://vignette.wikia.nocookie.net/vikingstv/images/6/63/Athelstan_S01P02.jpg/revision/latest/scale-to-width-down/310?cb=20140126070301ttps://pbs.twimg.com/profile_images/562466745340817408/_nIu8KHX.jpeg',
+		scores: [
+			'3',
+			'1',
+			'2',
+			'1',
+			'3',
+			'2',
+			'1',
+			'3',
+			'3',
+			'3'
+		]
+	},
+	{
+		name: 'Rollo',
+		photo: 'https://vignette.wikia.nocookie.net/vikingstv/images/0/00/4x17_Rollo_Picture.jpeg/revision/latest/scale-to-width-down/310?cb=20170108232544',
+		scores: [
+			'1',
+			'1',
+			'3',
+			'3',
+			'3',
+			'3',
+			'3',
+			'3',
+			'3',
+			'1'
+		]
+	},
+	{
+		name: 'Bjorn',
+		photo: 'https://vignette.wikia.nocookie.net/vikingstv/images/c/c2/Bjorn_S04E20_promo.jpg/revision/latest/scale-to-width-down/310?cb=20170128133028',
+		scores: [
+			'1',
+			'1',
+			'3',
+			'1',
+			'3',
+			'2',
+			'3',
+			'1',
+			'1',
+			'3'
+		]
+	},
+	{
+		name: 'Ivar',
+		photo: 'https://vignette.wikia.nocookie.net/vikingstv/images/5/5d/Ivar_HD_5x10.jpg/revision/latest?cb=20180126055221',
+		scores: [
+			'3',
+			'2',
+			'2',
+			'2',
+			'2',
+			'2',
+			'3',
+			'2',
+			'3',
+			'3'
+		]
+	},
+	{
+		name: 'Ecbert',
+		photo: 'https://vignette.wikia.nocookie.net/vikingstv/images/2/27/Ecbertseason3.jpg/revision/latest/scale-to-width-down/310?cb=20150307192342https://i1.imgiz.com/data/videoshots/6829/6829475-6461.jpg',
+		scores: [
+			'3',
+			'2',
+			'3',
+			'1',
+			'3',
+			'1',
+			'3',
+			'1',
+			'1',
+			'3'
+		]
+	},
+	{
+		name: 'The Seer',
+		photo: 'https://vignette.wikia.nocookie.net/vikingstv/images/2/21/Seerseason3.jpg/revision/latest/scale-to-width-down/310?cb=20150307192155',
+		scores: [
+			'3',
+			'3',
+			'3',
+			'3',
+			'3',
+			'3',
+			'3',
+			'3',
+			'3',
+			'3'
+		]
+	},
+	{
+		name: 'Aethelwulf',
+		photo: 'https://vignette.wikia.nocookie.net/vikingstv/images/2/23/Aethelwulf_VIKINGS.S5/revision/latest/scale-to-width-down/310?cb=20171216084239',
+		scores: [
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1'
+		]
+	}
+];
+
+module.exports = friendsArray;
